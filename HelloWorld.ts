5be2eac59dc63af0964404d183a878262c0011db
Muthu Viswanathan <muthuviswanath@outlook.com>
2022-06-13 16:41:59 +0530

    World

---
+++ HelloWorld.ts
@@ -0,0 +1,19 @@
+let a: number = 100; // output - 100
+let b: number = 12.56; // output - 12.56
+let c: number = 0b1010; //output - 10
+let d: number = 0x12AB;
+let e: number = 0o77;
+
+console.log("Value of a: " + a);
+console.log("Value of b: " + b);
+console.log("Value of c: " + c);
+console.log("Value of d: " + d);
+console.log("Value of e: " + e);
+
+
+let names: string ='Muthu';
+let designation: string = "Sales Manager";
+let finalstr: string = `${names} is woring as ${designation}`;
+
+console.log(finalstr);
+Testing
